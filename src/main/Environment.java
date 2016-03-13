35eeb499a3c40343d90476b6140798ecf4c16ef7
jorgegvalencia <jorgegvalencia@gmail.com>
2016-03-13 14:41:29 +0100

    Clases de test de la normalización y las opciones --UDA y --nomap de metamap16.

--- src/main/Environment.java
+++ src/main/Environment.java
@@ -7,7 +7,7 @@ public final class Environment {
 	//+ "cell,fish,ftcn,geoa,idcn,inpr,menp,mnob,podg,qlco,qnco,spco,tmco "
 	// "aapp"
 	public static String METAMAP_HOST = "192.168.33.10"; //"luria.dia.fi.upm.es"
-	public static String METAMAP_OPTIONS = "-y --restrict_to_sts resa,podg,horm,diap,dsyn,topp,chvf,neop,fndg,hlca -R SNOMEDCT_US";
+	public static String METAMAP_OPTIONS = "-y --restrict_to_sts resa,podg,horm,diap,dsyn,topp,chvf,neop,fndg,hlca -R SNOMEDCT_US"; // --UDA UDAfile --nomap Exclusions
 	public static String TRIALS_PATH = "data/trials/";
 	public static ArrayList<String> XML_ATTRIBUTES = new ArrayList<>(Arrays.asList("study_type","gender","url","overall_status","lastchanged_date","start_date"));
 }
--- src/test/NormalizationTest.java
+++ src/test/NormalizationTest.java
@@ -1,13 +1,19 @@
 package test;
 
+import java.util.List;
+
 import normalization.CoreDatasetServiceClient;
 
 public class NormalizationTest {
 
 	public static void main(String[] args) {
-		CoreDatasetServiceClient normclient = new CoreDatasetServiceClient();
-		String normalform = normclient.getNormalFormAsString("116154003", true);
-		System.out.println(normalform);
+		CoreDatasetServiceClient normclient = CoreDatasetServiceClient.getInstance();
+		//String normalform = normclient.getNormalFormAsString("126926005", true);
+		//System.out.println(normalform);
+		List<String> padres = normclient.getParents("116154003");
+		for(String padre: padres){
+			System.out.println(padre);
+		}
 	}
 
 }
